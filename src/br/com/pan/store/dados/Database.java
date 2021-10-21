d4b518ae2347bdf26378b36cb1e28d6d718f90ed
jonaslucenafilho <jonaslucena@outlook.com>
2021-10-21 19:45:19 -0300

    criando a classe Database

---
+++ src/br/com/pan/store/dados/Database.java
@@ -0,0 +1,18 @@
+package br.com.pan.store.dados;
+
+import java.util.Map;
+
+import br.com.pan.store.entidades.FormaDePagamento;
+import br.com.pan.store.entidades.Produto;
+
+public class Database {
+	
+	private Map<Integer, Produto> produtos;
+	private Map<Integer, FormaDePagamento> formasPagamentos;
+	
+	public void popularProdutos() {
+//		Produto p1 = new Produto(1, );
+//		Produto p2 = new Produto(2, );
+//		Produto p3 = new Produto(3, );
+	}
+}
