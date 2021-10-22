3d3c42f2691bb139a5277213ab2ffb0dd4c581ee
Marcos Paulo <mpcc.show@gmail.com>
2021-10-21 21:32:17 -0300

    implementação inicial da classe Carrinho

--- src/br/com/pan/store/entidades/Carrinho.java
+++ src/br/com/pan/store/entidades/Carrinho.java
@@ -1,5 +1,25 @@
 package br.com.pan.store.entidades;
 
-public class Carrinho {
+import java.util.ArrayList;
 
+public class Carrinho {
+    private Integer id;
+    private String nomeCliente;
+    private FormaDePagamento formaPagamento;
+    private Integer notaFiscal;
+    private ArrayList<CarrinhoItem> itens;
+
+    public Carrinho(Integer id, String nomeCliente, FormaDePagamento formaPagamento) {
+        this.id = id;
+        this.nomeCliente = nomeCliente;
+        this.formaPagamento = formaPagamento;
+    }
+
+    public void AdicionarItem(CarrinhoItem carrinhoItem){
+        itens.add(carrinhoItem);
+    }
+
+    public void GerarCupomFiscal(){
+        System.out.println("CUPOM FISCAL");
+    }
 }
