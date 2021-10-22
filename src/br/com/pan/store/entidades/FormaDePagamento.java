d22f5ff760afb461676ec782bfba644854f32d2b
Carlos Betiol <carlos@sistelpa.com>
2021-10-21 21:32:12 -0300

    trocado produtoId para produto na classe CarrinhoItem no diagrama e implementado a classe abstrata FormaDePagamento

--- src/br/com/pan/store/entidades/FormaDePagamento.java
+++ src/br/com/pan/store/entidades/FormaDePagamento.java
@@ -1,5 +1,30 @@
 package br.com.pan.store.entidades;
 
-public class FormaDePagamento {
+public abstract class FormaDePagamento {
+
+    private Integer id;
+    private String nome;
+    private Integer nParcelas;
+    private Double taxaJuros;
+
+    public Double calculaValor() {
+        return null;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getnParcelas() {
+        return nParcelas;
+    }
+
+    public Double getTaxaJuros() {
+        return taxaJuros;
+    }
 
 }
