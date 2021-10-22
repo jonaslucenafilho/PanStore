6ebc377c3e56b6b67c2a26dd43f5456e0d3db117
Jonas Lucena <jonaslucena@outlook.com>
2021-10-22 20:14:20 -0300

    classe PagamentoDinheiro implementada

---
+++ src/br/com/pan/store/entidades/PagamentoDinheiro.java
@@ -0,0 +1,19 @@
+package br.com.pan.store.entidades;
+
+public class PagamentoDinheiro extends FormaDePagamento {
+
+	public PagamentoDinheiro(Integer id, String nome, Integer nParcelas, Double taxaJuros) {
+		super(id, nome, nParcelas, taxaJuros);
+	}
+
+	@Override
+	public Double calculaValor(Carrinho carrinho) {
+		Double total = 0.0;
+		
+		for (CarrinhoItem item : carrinho.getItens()) {	
+			total = item.getQuantidade() * item.getPrecoVendido();		
+		}
+		
+		return total - total * (this.getDesconto() / 100);
+	}	
+}
