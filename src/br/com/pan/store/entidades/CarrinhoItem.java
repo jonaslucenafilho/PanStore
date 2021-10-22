168e3274e6c2d09ec5fcb3591138d1df57655a82
Marcos Paulo <mpcc.show@gmail.com>
2021-10-21 21:24:49 -0300

    classe CarrinhoItem implementada

--- src/br/com/pan/store/entidades/CarrinhoItem.java
+++ src/br/com/pan/store/entidades/CarrinhoItem.java
@@ -1,4 +1,13 @@
 package br.com.pan.store.entidades;
 
 public class CarrinhoItem {
+    private Produto produto;
+    private Double quantidade;
+    private Double precoVendido;
+
+    public CarrinhoItem(Produto produto, Double quantidade, Double precoVendido) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.precoVendido = precoVendido;
+    }
 }
